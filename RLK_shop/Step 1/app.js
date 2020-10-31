12e8d3a40c58a7228df819513cd410fca0de9a54
ModasDev <mr.dubovchuk@gmail.com>
2020-10-31 19:26:13 +0200

    finished step 1

--- RLK_shop/Step 1/app.js
+++
@@ -1,7 +0,0 @@
-//../../node_modules/nodemon/bin/nodemon.js app.js
-//ctrl + c -  выход из 
-
-
-let a = [1,2,'Hello',3,5,9,10];
-
-console.log(a.length);
---
+++ RLK_shop/Step 1/app_1.js
@@ -0,0 +1,18 @@
+/*
+ 1 Устанавливаем nodemon 
+ 2 npm install nodemon
+ 3 ctrl + C - Выход из node 
+*/
+
+
+//../../node_modules/nodemon/bin/nodemon.js app.js
+//ctrl + c -  выход из 
+
+
+let a = [1,2,'Hello',3,5];
+
+console.log(a.length);
+
+for(let i = 0; i<a.length;i++){
+    console.log(a[i]);
+}
