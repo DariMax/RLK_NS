b1f02546aff14e99526184e5cda3fbb7738c2503
ModasDev <mr.dubovchuk@gmail.com>
2020-11-14 16:23:37 +0200

    added two steps , prepare app for the work

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 //подключаем установленный модуль через команду requre 
 let express = require('express');
+//Новая версия подключения 
+//"type":"module", - добавить в package.json
+//import express from "express";
 
 //создаем новый объект экземпляр класса express
 //далее все манипуляции будут производится с помощью 
 //этого приложения app (его имя может быть любое)
 let app = express();
-let port  = 8080;
+let port  = 3000;
 
 //public - имя папки где хранится статика
 app.use(express.static('public'));
+//Задаем шаблонизатор
+//прописваем путь до папки
+//app.set('views','./views');
+//Запуск шаблонизатора
+app.set('view engine','pug');
+
 
 app.listen(port ,function(){
-    console.log('node express work on 8080');
+    console.log('node express work on 3000');
 });
 
 //Далее, чтобы запустить первое приложение нужно:
-//get - метод обращения, первый параметр url адрес куда идет обращение
+//get - метод обращения, первый параметр url адрес куда идет обращение (путь)
 // у функции будет два параметра запрос и ответ
 app.get('/',function(req,res){
+    //console.log(req.params);
     //ответим
     //res.end('hello');
-      res.render('index.html');
+   res.render('index');
 }); 
 
 app.get('/cat',function(req,res){
-    // res.end('cat');
-    res.render('category.html');
+  
 });
